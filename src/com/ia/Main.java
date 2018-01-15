dd8c560a4dbcdec4cbe117a982e7a2c5deab458c
Bioanas <33870208+Bioanas@users.noreply.github.com>
2018-01-15 17:18:03 +0200

    Initial commit

---
+++ src/com/ia/Main.java
@@ -0,0 +1,22 @@
+package com.ia;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x=0;
+	int y=1;
+	if (x>0){
+	    x=3;
+        System.out.println(x);
+        if (x==y){
+            //logic goes here
+        }
+        else if  (x==0){
+            //logic goes here
+        }
+        else{
+         //logic goes here
+        }
+    }
+    }
+}
